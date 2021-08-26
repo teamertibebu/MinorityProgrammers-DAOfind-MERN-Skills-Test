f98a9bb5cfbb749b72bfb6efa10f91b6837feb10
Teamer Tibebu <teamertibebu@gmail.com>
2021-08-26 14:45:53 -0500

    (add) route to reference Dao & Socials schemas - one time use

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const Dao = require('./db/Models/DAO');
+const Socials = require('./db/Models/Socials');
 const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build', 'index.html')));
@@ -29,6 +30,31 @@ app.get('/dao/:id', (req, res) => {
     }
   });
 });
+
+///Below was used to reference the Dao & Socials schemas/////////
+/*app.get('/update', async (req, res) => {
+  const daos = await Dao.find({});
+  const socials = await Socials.find({});
+  daos.forEach((dao) => {
+    socials.forEach((social) => {
+      if (dao.full_name === social.dao_name) {
+        dao.socials = social._id;
+        social.dao_id = dao._id;
+
+        dao
+          .save()
+          .then(() => console.log('saved Dao'))
+          .catch((error) => console.log('Error Dao: ', error));
+        social
+          .save()
+          .then(() => console.log('saved Social'))
+          .catch((error) => console.log('Error Social: ', error));
+      }
+    });
+  });
+  res.json(socials);
+});
+*/
 
 app.listen(PORT, (err) => {
   console.log(err ? err : `http://localhost:${PORT}`);
