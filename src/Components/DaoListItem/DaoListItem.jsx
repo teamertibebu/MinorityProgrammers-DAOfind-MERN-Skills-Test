714c56535fbee3bf77421f00fbcc7edc930d74d2
Teamer Tibebu <teamertibebu@gmail.com>
2021-08-26 16:41:29 -0500

    (update) daoListItem to handle Accordion dropdown & daoPage expansion

--- src/Components/DaoListItem/DaoListItem.jsx
+++ src/Components/DaoListItem/DaoListItem.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import { Link, useHistory } from 'react-router-dom';
+import {
+  Grid,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 
 const DaoListItem = ({ dao }) => {
   ///For Development Only:////////////////
@@ -13,38 +20,41 @@ const DaoListItem = ({ dao }) => {
 
   const handleDaoClick = (id, e) => {
     history.push(`/dao/${id}`);
+    console.log('click');
   };
 
   return (
-    <Grid
-      item
-      container
-      xs={12}
-      direction="row"
-      alignItems="center"
-      style={{
-        backgroundColor: 'white',
-        border: '1px solid black',
-        padding: '20px',
-        margin: '7px',
-      }}
-      onClick={handleDaoClick.bind(this, dao._id)}
-    >
-      <Grid item xs={2}>
-        <img src={dao.logo_link} style={{ width: '20%' }} alt="dao logo" />
-      </Grid>
-      <Grid item xs={2}>
-        <p>{dao.full_name}</p>
-      </Grid>
-      <Grid item xs={2}>
-        <p>{dao.category}</p>
-      </Grid>
-      <Grid item xs={2}>
-        <p>{dao.aum}</p>
-      </Grid>
-      <Grid item xs={2}>
-        <p>{date}</p>
-      </Grid>
+    <Grid item container xs={12} direction="column">
+      <Accordion
+        style={{
+          backgroundColor: 'white',
+          border: '1px solid black',
+          padding: '20px',
+          margin: '7px',
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Grid item xs={2}>
+            <img src={dao.logo_link} style={{ width: '20%' }} alt="dao logo" />
+          </Grid>
+          <Grid item xs={2}>
+            <p>{dao.full_name}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{dao.category}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{dao.aum}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{date}</p>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>{dao.full_name}</div>
+          <OpenInNewIcon onClick={handleDaoClick.bind(this, dao._id)} />
+        </AccordionDetails>
+      </Accordion>
     </Grid>
   );
 };
